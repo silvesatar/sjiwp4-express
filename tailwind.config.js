2248e570839477e88fd236d86c7bc0047acf55fb
Silvestar P <silvestar.puncikar@gmail.com>
2023-12-21 09:21:49 +0100

    signup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.ejs"],
+  content: ["./views/**/*.handlebars"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
